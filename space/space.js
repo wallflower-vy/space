d75274c373aee95bff2b1465872f9943e252f09f
wallflower_vy <owoadevic@gmail.com>
2022-01-18 06:42:27 -0800

    commit

---
+++ space/space.js
@@ -0,0 +1,15 @@
+function showsidenav(){
+    document.getElementById('nav-item-box').style.display = "block";
+    document.getElementById('show-menu').style.display = "none";
+    document.getElementById('close').style.display = "block";
+   
+}
+function hideidenav(){
+    document.getElementById('nav-item-box').style.display = "none";
+    document.getElementById('close').style.display = "none";
+    document.getElementById('show-menu').style.display = "block";
+}
+
+ 
+// hideidenav();
+// showsidenav();
